1256ed4602ae16ddf69314904ccff5d1b1c55489
BiQiB7 <fongbiqi@gmail.com>
2023-12-26 02:05:18 +0800

    test

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BiqiPortfolioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'biqi_portfolio'
---
+++ mythical_creatures/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MythicalCreaturesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mythical_creatures'
